d7bc866f5b59d070ab74a144d59c060de5839fa6
saicOo <sayed.ahmed.elbadry@gmail.com>
2024-09-10 15:51:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  // Function to check if the video is in view
+  function checkVideoInView() {
+    $("video").each(function () {
+      var video = $(this);
+      var videoTop = video.offset().top;
+      var videoBottom = videoTop + video.outerHeight();
+      var windowTop = $(window).scrollTop();
+      var windowBottom = windowTop + $(window).height();
+
+      // Check if the video is fully visible in the viewport
+      if (videoBottom >= windowTop && videoTop <= windowBottom) {
+        video[0].play(); // Play video if in view
+      } else {
+        video[0].pause(); // Pause video if out of view
+      }
+    });
+  }
+
+  // Initial check
+  checkVideoInView();
+
+  // Check video position on scroll
+  $(".reelsContainer").on("scroll", function () {
+    checkVideoInView();
+  });
+});
